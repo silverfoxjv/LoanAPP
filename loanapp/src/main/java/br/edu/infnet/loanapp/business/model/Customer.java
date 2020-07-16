2f1019f0c599fb7963859b4ad72e0595107e1beb
Joao Dias <joao.dias@temasistemas.com.br>
2020-07-15 22:36:31 -0300

    Criação do Pacote

---
+++ loanapp/src/main/java/br/edu/infnet/loanapp/business/model/Customer.java
@@ -0,0 +1,9 @@
+package br.edu.infnet.loanapp.business.model;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+
+	private static final long serialVersionUID = 2469110101808443358L;
+
+}

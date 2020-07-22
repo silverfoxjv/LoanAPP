1965b8665f4c6d89d22a20937c98c7df6c6fdd43
Ben Sauer <Ben Sauer@DESKTOP-BG74MIU>
2020-07-21 21:25:41 -0300

    Reestruturação do script SQL e adição da classe Installment(Parcela)

--- loanapp/scripts/estrutura.sql
+++ loanapp/scripts/estrutura.sql
@@ -19,8 +19,8 @@ create table LN_CONTRACT(
     startDate date not null,
     interestRate DECIMAL(13,2) not null,
     loanAmount DECIMAL(13,2) not null,
+    installments int not null,
     loanPmentAmountDue DECIMAL(13,2) not null,
-    loanPmentAmountDate date not null,
     FOREIGN KEY (customerId) references LN_CUSTOMER(id),
     FOREIGN KEY (collectorId) references LN_COLLECTOR(id)
 );
@@ -29,8 +29,17 @@ create table LN_PAYMENT(
     id int primary key AUTO_INCREMENT,
     contractId int not null,
     paymentDate date not null,
-    paymentMonth int not null,
+    installmentNbr int not null,
     capitalPaid DECIMAL(13,2) null,
     interestPaid DECIMAL(13,2) null,
     FOREIGN KEY (contractId) references LN_CONTRACT(id)
+);
+
+create table LN_INSTALLMENT(
+	id int primary key AUTO_INCREMENT,
+	contractId int not null,
+	interestInd DECIMAL(13,2) null,
+	capitalInd DECIMAL(13,2) null,
+	installmentDateDue date not null,
+	FOREIGN KEY (contractId) references LN_CONTRACT(id)
 );
---
+++ loanapp/src/main/java/br/edu/infnet/loanapp/business/model/Installment.java
@@ -0,0 +1,16 @@
+package br.edu.infnet.loanapp.business.model;
+
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "LN_INSTALLMENT")
+public class Installment implements Serializable {
+
+	private static final long serialVersionUID = 104299398955263373L;
+	
+}
